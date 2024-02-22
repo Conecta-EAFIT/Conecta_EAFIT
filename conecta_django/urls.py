14936e76216b632f996c1fab42dda0cd7f9d6491
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-02-22 10:36:49 -0500

    Agregando las migraciones de base de datos

--- conecta_django/urls.py
+++ conecta_django/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from profesor import views as profesorViews
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', profesorViews.home),
     path('about/', profesorViews.about),
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
