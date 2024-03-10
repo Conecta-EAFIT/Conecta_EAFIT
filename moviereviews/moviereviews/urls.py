0d9f0cda0d9a14504e20ff30fe30481378b4f85f
MADiazV1 <diazvalenciam9@gmail.com>
2024-03-09 20:06:35 -0500

    Taller 2 terminado

--- moviereviews/moviereviews/urls.py
+++ moviereviews/moviereviews/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', movieviews.home),
+    path('', movieviews.home, name='home'),
     path('about/', movieviews.about),
     path('news/', include('news.urls')),
 ]
