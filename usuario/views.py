c702443aca687ba9f5db80ec0bf66f498e021140
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-05-02 11:24:04 -0500

    Allowed hosts

---
+++ usuario/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .models import Administrador, Estudiante
+
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request)
+            # Verificar el tipo de usuario
+            if Administrador.objects.filter(usuario=user).exists():
+                return redirect('profesor:conectaHome_admin')
+            elif Estudiante.objects.filter(usuario=user).exists():
+                return redirect('profesor:conectaHome')
+            else:
+                # Manejar otros tipos de usuarios si es necesario
+                pass
+        else:
+            # Manejar autenticación fallida
+            pass
+    return render(request, 'login.html')
