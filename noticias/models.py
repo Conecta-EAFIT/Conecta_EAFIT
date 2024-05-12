8c5e9a3be86b0c6cb9fcf0208a3c4f8fb0305a60
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-05-11 21:48:47 -0500

    Funcionalidad de Autores de noticias agregados

--- noticias/models.py
+++ noticias/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from profesor.models import Profesor
 
 # Create your models here.
 class Noticias(models.Model):
     headline = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateField()
+    author = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True, related_name='noticias')
 
     def __str__(self): return self.headline
