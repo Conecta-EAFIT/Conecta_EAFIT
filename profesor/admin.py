dba0548b4378a2b85cb13f9d26fad3bc415052f2
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-05-27 22:29:22 -0500

    Agregamos mas info a la BD

--- profesor/admin.py
+++ profesor/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Profesor, Carrera
+from .models import Profesor, Carrera, Comentario, Voto
 
 
 
 # Register your models here.
 admin.site.register(Profesor)
 admin.site.register(Carrera)
+admin.site.register(Comentario)
+admin.site.register(Voto)
 
